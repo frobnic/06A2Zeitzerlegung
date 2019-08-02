7c40f4587e0fb52eaf2f1fef382fc7dc8e4d3fbe
frank paulsen <frobnic+ita@gmail.com>
2019-08-02 08:48:55 +0200

    code prettyfied

--- 06A2Zeitzerlegung/Program.cs
+++ 06A2Zeitzerlegung/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _06A2Zeitzerlegung
 {
@@ -10,6 +6,28 @@ namespace _06A2Zeitzerlegung
     {
         static void Main(string[] args)
         {
+            const int SproTag = 86400;// Sekunden pro Tag
+            const int SproStunde = 3600; // Sekunden pro Stunde
+            const int SproMinute = 60;   // Sekunden pro Minute
+            int eingabe, sekunden, minuten, stunden, tage, wert;
+
+            Console.Write("Hallo, Sekunde mal! ");
+            //if (int.TryParse(Console.ReadLine(), out eingabe))
+            for (eingabe = 1; eingabe < 86402; eingabe++)
+            {
+                wert = eingabe;
+
+                tage = wert / (SproTag);
+                wert = wert - (tage * SproTag);
+
+                stunden = wert / (SproStunde);
+                wert = wert - (stunden * SproStunde);
+
+                minuten = wert / SproMinute;
+                sekunden = wert - (minuten * SproMinute);
+
+                Console.WriteLine("{0} Sekunden sind {1} Tag(e), {2} Stunden, {3} Minute(n) und {4} Sekunde(n). ", eingabe, tage, stunden, minuten, sekunden);
+            }
         }
     }
 }
